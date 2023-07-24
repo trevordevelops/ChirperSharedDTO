b28c474b4e807e7f1fa76f7503c3015a93b53a01
Trevor <85640211+trevordevelops@users.noreply.github.com>
2023-07-24 15:39:00 -0700

    Added postedToUID to ChirpResponseDTO

--- Sources/ChirperSharedDTO/ChirpResponseDTO.swift
+++ Sources/ChirperSharedDTO/ChirpResponseDTO.swift
@@ -11,10 +11,12 @@ public struct ChirpResponseDTO: Identifiable, Codable, Hashable {
 	public var id: UUID
 	public var message: String
 	public var postedDate: Date
+	public var postedToUID: String
 
-	public init(id: UUID, message: String, postedDate: Date) {
+	public init(id: UUID, message: String, postedDate: Date, postedToUID: String) {
 		self.id = id
 		self.message = message
 		self.postedDate = postedDate
+		self.postedToUID = postedToUID
 	}
 }
